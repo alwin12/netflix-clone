94b9e624c0177d6bd5db690939660c5987d1d61b
alwin <alwinsunny123456@gmail.com>
2021-04-10 19:36:49 +1000

    initial

---
+++ src/global-styles.js
@@ -0,0 +1,20 @@
+import {createGlobalStyle} from 'styled-components'
+
+
+
+
+export const GlobalStyles = createGlobalStyle`
+
+
+html,body{
+    font-family: 'Helvetica Neue' , Helvetica ,Arial, sans-sherif;
+    -webkit-font-smoothing:antialised;
+    -moz-osx-font-smoothing:grayscale;
+    background-color:black;
+    color:#333333;
+    font-size:16px;
+}
+
+
+
+`
